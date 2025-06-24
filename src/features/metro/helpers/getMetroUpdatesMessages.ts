6189aea506eb582572fa70f90396239ca0398a15
GroovyGab <ghernandezlohaus.78@gmail.com>
2025-06-24 19:29:28 -0400

    fix: arreglar getMetroUpdatesMessages

--- src/features/metro/helpers/getMetroUpdatesMessages.ts
+++ src/features/metro/helpers/getMetroUpdatesMessages.ts
@@ -1,5 +1,10 @@
 import { container } from '@sapphire/framework';
 
 export async function getMetroUpdatesMessages(guildId?: string) {
+	if (guildId) {
+		const data = await container.prisma.guildConfig.findUnique({ where: { guildId: guildId }, select: { metroStatusMessages: true } });
+		return data ? data.metroStatusMessages : [];
+	}
+
 	return await container.prisma.metroStatusMessage.findMany(guildId ? { where: { guildId: guildId } } : undefined);
 }
