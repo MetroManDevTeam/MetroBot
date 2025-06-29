cc2c8c4de55e30a8793121a29c202eb62344b133
GroovyGab <ghernandezlohaus.78@gmail.com>
2025-06-28 22:40:05 -0400

    feat: Guardar datos de estaciones en db (#6)

--- src/scheduled-tasks/station-details-update.ts
+++ src/scheduled-tasks/station-details-update.ts
@@ -1,3 +1,5 @@
+import { Time } from '@sapphire/time-utilities';
+import { sleep } from '#utils/promise/sleep';
 import { ApplyOptions } from '@sapphire/decorators';
 import { ScheduledTask } from '@sapphire/plugin-scheduled-tasks';
 
@@ -10,8 +12,41 @@ import { ScheduledTask } from '@sapphire/plugin-scheduled-tasks';
 })
 export class UserTask extends ScheduledTask {
 	public override async run() {
-		/**
-		 * @todo guardar detalles sobre estaciones en db periodicamente
-		 */
+		this.container.logger.info('[MetroStationDetials] Actualizando datos...');
+		const statusInfo = await this.container.metro.getMetroNetworkStatus();
+		const promises = [];
+
+		for (const line of Object.values(statusInfo)) {
+			for (const station of line.stations) {
+				await sleep(Time.Second);
+
+				this.container.logger.info(`[MetroStationDetials] Actualizando datos de la estación ${station.name}[${station.code}]`);
+
+				const stationData = await this.container.metro.getMetroStationDetails(station.code);
+
+				const promise = this.container.prisma.metroStationDetails.upsert({
+					where: { code: stationData.code },
+					create: {
+						code: stationData.code,
+						name: stationData.name,
+						line: stationData.line,
+						expressRoute: stationData.expressRoute
+					},
+					update: { name: stationData.name, line: stationData.line, expressRoute: stationData.expressRoute }
+				});
+
+				promises.push(promise);
+			}
+		}
+
+		await Promise.all(promises);
+
+		this.container.logger.info('[MetroStationDetials] Datos actualizados correctamente');
+	}
+}
+
+declare module '@sapphire/plugin-scheduled-tasks' {
+	interface ScheduledTasks {
+		'station-details-update': never;
 	}
 }
