fae3d3d20813fa73154d6bb3c5db2916cf978246
MetroMan <services@metroman.me>
2025-07-11 14:26:28 -0400

    Update AccessibilityChangeDetector.js

--- modules/metro/core/services/AccessibilityChangeDetector.js
+++ modules/metro/core/services/AccessibilityChangeDetector.js
@@ -360,7 +360,10 @@ class AccessibilityChangeDetector {
                 config.lastUpdated = new Date().toISOString();
                 
                 // Save config
-                await accessCore.saveAccessConfig(`${stationKey} ${line.replace('l','')}`, config);
+                const existingLines = ['l1', 'l2', 'l3', 'l4', 'l4a', 'l5', 'l6'];
+const line = existingLines.some(l => stationKey.includes(l)) ? '' : ` ${line}`;
+
+await accessCore.saveAccessConfig(`${stationKey}${line}`, config);
             }
             
             this.logger.info(`Processed ${changes.length} changes`);
